dc378d3c2b13360fe264a64d953df0733ae94a38
shellieD <shellie.downie13@gmail.com>
2021-11-24 11:50:54 +0000

    Add main heading and introduction to README.md file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
-
+    
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "levelOne") {
@@ -13,14 +13,14 @@ document.addEventListener("DOMContentLoaded", function() {
                     runLevelTwo();
                 } else {
                     if (this.getAttribute("data-type") === "levelThree") {
-                    runLevelThree();
+                        runLevelThree();
                     }   
                 }
             }
         })
     }
 })
-
+    
 //function runGame() {
   //  let difficulty = {
     //    levelOneWords: ['Alien', 'Asteroid', 'Astronaut', 'Big Bang', 'Black Hole', 'Comet', 'Cosmic', 'Crater', 'Crew', 'Danger', 'Descent', 'Disaster', 'Distant', 'Dwarf Planet', 'Earth', 'Full Moon', 'Galaxy', 'Gravity', 'Horizon', 'Hubble', 'Impact', 'Jupiter', 'Kilometers', 'Launch', 'Launch Pad', 'Light Years', 'Lunar', 'Magnetic', 'Mars', 'Mercury', 'Meteor', 'Meteorite', 'Milky Way', 'Moon', 'NASA', 'Nebula', 'Newton', 'North Star', 'Nova', 'Neptune', 'Orbit', 'Outer-Space', 'Ozone', 'Particles', 'Planet', 'Probe', 'Proton', 'Quest', 'Rocket', 'Rotation', 'Science', 'Scientist', 'Shooting Star', 'Shuttle', 'Solar', 'Solar System', 'Space', 'Space Craft', 'Space Station', 'Star Dust', 'Stars', 'Stellar', 'Storms', 'Saturn', 'Twinkle', 'Universe', 'Uranus', 'Venus', 'Voyage'],
