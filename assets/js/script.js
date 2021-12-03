49745e649fdbee8563082a5fea7cc414ab697e46
shellieD <shellie.downie13@gmail.com>
2021-12-03 02:12:27 +0000

    fix typo

--- assets/js/script.js
+++ assets/js/script.js
@@ -193,7 +193,7 @@ function decreaseLives () {
                 let audio = document.getElementById('powerdown');
                 setTimeout(rocketSound(audio), 1000);
             } 
-            document.getElementById('rocket').classList.add('animation-shake');  //NEED TO CREATE A NEW ANIMATION FOR THIS ROCKET
+            document.getElementById('rocket').classList.add('animation-shake'); 
             setTimeout(missionAborted, 3500);
     }
 }
@@ -211,7 +211,7 @@ function checkWord() {
         incrementScore();
         if (soundOn === true) {
             let audio = document.getElementById('takeoff');
-           setTimeout(rocketSound(audio), 1000);
+            setTimeout(rocketSound(audio), 1000);
         } 
         document.getElementById('rocket').classList.add('animation'); 
         setTimeout(missionAccomplished, 3500);
